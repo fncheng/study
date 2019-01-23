d3409fcea225beba1eda42da70e522762dba5091
i-dongcheng <qq862647669@outlook.com>
2019-01-23 09:57:58 +0800

    Add a helper class SqlHelper

--- Test/Controllers/UserController.cs
+++ Test/Controllers/UserController.cs
@@ -14,8 +14,11 @@ namespace Test.Controllers
     public class UserController : Controller
     {
 
-        public const string constr = @"Data Source=SANDBOXV2DEV142;Initial Catalog=UserInfo;Integrated Security=True";
+        public const string constr = @"Data Source=.;Initial Catalog=UserInfo;User ID=dongcheng;Password=Aa336699";
         public static List<User> regionUserInfoList { get; set; }
+
+        SqlHelper sqlhelp = new SqlHelper();
+
         // GET: User
         #region Index
         public ActionResult Index()//Index
@@ -29,7 +32,7 @@ namespace Test.Controllers
             }
             else
             {
-                string text = "userinfo_Get"; //name of Stored procedure
+                string text = "userinfo_get"; //name of Stored procedure
                 SqlConnection conn = new SqlConnection(constr);
                 SqlCommand cmd = new SqlCommand(text, conn);
                 cmd.CommandType = CommandType.StoredProcedure;//Declaring that you want to execute is a stored procedure
@@ -68,63 +71,24 @@ namespace Test.Controllers
         #region AddInfo
         public ActionResult AddInfo()
         {
-
             return View();
         }
         [HttpPost]
         public ActionResult AddInfo1()
         {
-            string text = "userinfo_add";
-            var test = Request.Form["UserName"].ToString();
-            SqlParameter[] addpara =
-            {
-                new SqlParameter("@name",Request.Form["UserName"].ToString()),
-                new SqlParameter("@sex",Request.Form["UserSex"].ToString()),
-                new SqlParameter("@age",Convert.ToInt16(Request.Form["UserAge"]))
-            };
-
-            SqlConnection conn = new SqlConnection(constr);
-            SqlCommand cmd = new SqlCommand(text, conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            foreach (SqlParameter parameter in addpara)
-            {
-                if (parameter != null)
-                    cmd.Parameters.Add(parameter);
-                else
-                    cmd.Parameters.Add(0);
-            }
-            conn.Open();    //Open the sql
-
-            cmd.ExecuteNonQuery();//
-            conn.Close();
+            var test1 = Convert.ToString(Request.Form["UserName"]);
+            var test2 = Convert.ToString(Request.Form["UserSex"]);
+            var test3 = Convert.ToInt16(Request.Form["UserAge"]);
+            sqlhelp.AddHelper(test1, test2, test3);
             return View();
         }
         #endregion
 
-        //public ActionResult DelInfo(int id)
-        //{
-        //    //ViewBag.UserId = id;
-        //    return View();
-        //}
         #region DelInfo
         public ActionResult DelInfo1(int id)
         {
-            string text = "userinfo_del";
             var test = id;
-            SqlParameter[] delpara = {
-                new SqlParameter("@id",id)
-            };
-            SqlConnection conn = new SqlConnection(constr);
-            SqlCommand cmd = new SqlCommand(text, conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            foreach (SqlParameter parameter in delpara)
-            {
-                cmd.Parameters.Add(parameter);
-            }
-            conn.Open();    //Open the sql
-            cmd.ExecuteNonQuery();
-            conn.Close();
-
+            sqlhelp.DelHelper(test);
             return View();
         }
         #endregion
@@ -147,31 +111,11 @@ namespace Test.Controllers
         [HttpPost]
         public ActionResult UpdateInfo1(int id)
         {
-            string text = "userinfo_update";
-            var test = Convert.ToString(Request.Form["UserName"]);
-            SqlParameter[] updatepara =
-            {
-                new SqlParameter("@id",id),
-                new SqlParameter("@name",Request.Form["UserName"].ToString()),
-                new SqlParameter("@sex",Request.Form["UserSex"].ToString()),
-                new SqlParameter("@age",Convert.ToInt16(Request.Form["UserAge"]))
-            };
-            //updatepara[0].Value=
-
-            SqlConnection conn = new SqlConnection(constr);
-            SqlCommand cmd = new SqlCommand(text, conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            foreach (SqlParameter parameter in updatepara)
-            {
-                if (parameter != null)
-                    cmd.Parameters.Add(parameter);
-                else
-                    continue;
-            }
-            conn.Open();    //Open the sql
-            cmd.ExecuteNonQuery();
-            conn.Close();
-
+            var test0 = id;
+            var test1 = Convert.ToString(Request.Form["UserName"]);
+            var test2 = Convert.ToString(Request.Form["UserSex"]);
+            var test3 = Convert.ToInt16(Request.Form["UserAge"]);
+            sqlhelp.UpdateHelper(test0, test1, test2, test3);
             return View();
         }
         #endregion
@@ -180,20 +124,8 @@ namespace Test.Controllers
         [HttpPost]
         public ActionResult SelInfo()
         {
-            string spName = "userinfo_sel";
             var test = Request.Form["SelInfo"].ToString();
-            SqlParameter[] selpara =
-            {
-                new SqlParameter("@name",Request.Form["SelInfo"].ToString())
-            };
-            SqlConnection conn = new SqlConnection(constr);
-            SqlCommand cmd = new SqlCommand(spName, conn);
-            cmd.CommandType = CommandType.StoredProcedure;
-            conn.Open();    //Open the sql
-            foreach (SqlParameter parameter in selpara)
-            {
-                cmd.Parameters.Add(parameter);
-            }
+            var sellist=sqlhelp.SelHelper(test);
             ////
             //XmlReader reader = new XmlReader();
             //reader.Read();
@@ -204,20 +136,7 @@ namespace Test.Controllers
             //reader.Close();
             //return a;
             ////
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            List<User> list = new List<User>();
-            while (reader.Read())
-            {
-                User user = new User()
-                {
-                    Name = reader["Name"].ToString(),
-                    Sex = reader["Sex"].ToString(),
-                    Age = Convert.ToInt16(reader["Age"])
-                };
-                list.Add(user);
-            }
-            return View(list);
+            return View(sellist);
         }
         #endregion
         public  static void ToXml(DataTable dt)
@@ -228,8 +147,8 @@ namespace Test.Controllers
         public static void ReadXmlByDataSet()
         {
             DataSet ds = new DataSet();
-            ds.GetXml(@"D:\abk.xml");
         }
     }
 
+    
 }
---
+++ Test/Helper/SqlHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Test.Models;
+
+namespace Test.Helper
+{
+    public class SqlHelper
+    {
+        public const string constr = @"Data Source=.;Initial Catalog=UserInfo;User ID=dongcheng;Password=Aa336699";
+        SqlConnection conn = new SqlConnection(constr);
+
+        //Add
+        public void AddHelper(string test1, string test2, int test3)
+        {
+            string text = "userinfo_add";
+            SqlParameter[] addpara =
+           {
+                new SqlParameter("@name",test1),
+                new SqlParameter("@sex",test2),
+                new SqlParameter("@age",test3)
+            };
+            SqlCommand cmd = new SqlCommand(text, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach (SqlParameter parameter in addpara)
+            {
+                if (parameter != null)
+                    cmd.Parameters.Add(parameter);
+                else
+                    cmd.Parameters.Add(0);
+            }
+            conn.Open();    //Open the sql
+
+            cmd.ExecuteNonQuery();//
+            conn.Close();
+        }
+
+        //Delete
+        public void DelHelper(int id)
+        {
+            string text = "userinfo_del";
+            SqlParameter[] delpara = {
+                new SqlParameter("@id",id)
+            };
+            conn.Open();    //Open the sql
+            SqlCommand cmd = new SqlCommand(text, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach (SqlParameter parameter in delpara)
+            {
+                cmd.Parameters.Add(parameter);
+            }
+
+            cmd.ExecuteNonQuery();
+        }
+
+        //Update
+        public void UpdateHelper(int test, string test1, string test2, int test3)
+        {
+            string text = "userinfo_update";
+            SqlParameter[] updatepara =
+            {
+                new SqlParameter("@id",test),
+                new SqlParameter("@name",test1),
+                new SqlParameter("@sex",test2),
+                new SqlParameter("@age",test3)
+            };
+            SqlCommand cmd = new SqlCommand(text, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            foreach (SqlParameter parameter in updatepara)
+            {
+                if (parameter != null)
+                    cmd.Parameters.Add(parameter);
+                else
+                    continue;
+            }
+            conn.Open();    //Open the sql
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        //Get
+        public List<User> SelHelper(string test)
+        {
+            string spName = "userinfo_sel";
+            SqlParameter[] selpara =
+            {
+                new SqlParameter("@name",test)
+            };
+            SqlCommand cmd = new SqlCommand(spName, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            conn.Open();    //Open the sql
+            foreach (SqlParameter parameter in selpara)
+            {
+                cmd.Parameters.Add(parameter);
+            }
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<User> list = new List<User>();
+            while (reader.Read())
+            {
+                User user = new User()
+                {
+                    Name = reader["Name"].ToString(),
+                    Sex = reader["Sex"].ToString(),
+                    Age = Convert.ToInt16(reader["Age"])
+                };
+                list.Add(user);
+            }
+            return list;
+        }
+    }
+}
